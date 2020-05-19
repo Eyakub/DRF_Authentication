a38bf20f03fd91fc09373b58e21e8e97e4acb955
Eyakub <eyakubsorkar@gmail.com>
2020-05-19 12:44:11 +0600

    working on usermodule to create using DRF

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -2,23 +2,21 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework import status
+from rest_framework import viewsets
 
 from .models import Poll, Choice
-from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
 
 
-# class PollList(APIView):
-#     def get(self, request):
-#         polls = Poll.objects.all()[:20]
-#         data = PollSerializer(polls, many=True).data
-#         return Response(data)
+"""
+viewsets.ModelViewSet - when you are going to allow all or most of CRUD
+operations on a model
 
+generics.* - when you only want to allow some operations on a model
 
-# class PollDetail(APIView):
-#     def get(self, request, pk):
-#         poll = get_object_or_404(Poll, pk=pk)
-#         data = PollSerializer(poll).data
-#         return Response(data)
+APIView when you want to completely customize the behaviour
+"""
 
 """
 ListCreateAPIView - get a list of entities or create them. 
@@ -39,7 +37,9 @@ class PollDetail(generics.RetrieveDestroyAPIView):
 
 
 class ChoiceList(generics.ListCreateAPIView):
-    queryset = Choice.objects.all()
+    def get_queryset(self):
+        queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
+        return queryset
     serializer_class = ChoiceSerializer
 
 
@@ -48,5 +48,34 @@ class ChoiceList(generics.ListCreateAPIView):
 CreateAPIView - Allows creating entities, but not listing them
 Allows POST
 """
-class CreateVote(generics.CreateAPIView):
+# class CreateVote(generics.CreateAPIView):
+#     serializer_class = VoteSerializer
+
+
+class CreateVote(APIView):
     serializer_class = VoteSerializer
+
+    def post(self, request, pk, choice_pk):
+        voted_by = request.data.get('voted_by')
+        data = {'choice': choice_pk, 'poll': pk, 'voted_by': voted_by}
+        serializer = VoteSerializer(data=data)
+        if serializer.is_valid():
+            vote = serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+"""
+Viewsets
+"""
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+"""
+User
+"""
+class UserCreate(generics.CreateAPIView):
+    serializer_class = UserSerializer
--- polls/serializers.py
+++ polls/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+
 from .models import Poll, Choice, Vote
 
 
@@ -25,3 +27,18 @@ class PollSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
+class UserSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            email = validated_data['email'],
+            username = validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user

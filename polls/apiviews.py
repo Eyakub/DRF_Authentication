87c9beec3a661ec8cadd08345916e7d72e56050a
Eyakub <eyakubsorkar@gmail.com>
2020-05-19 11:12:27 +0600

    added the generics view

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -1,20 +1,52 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework import generics
 
 from .models import Poll, Choice
-from .serializers import PollSerializer
+from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
 
 
-class PollList(APIView):
-    def get(self, request):
-        polls = Poll.objects.all()[:20]
-        data = PollSerializer(polls, many=True).data
-        return Response(data)
+# class PollList(APIView):
+#     def get(self, request):
+#         polls = Poll.objects.all()[:20]
+#         data = PollSerializer(polls, many=True).data
+#         return Response(data)
 
 
-class PollDetail(APIView):
-    def get(self, request, pk):
-        poll = get_object_or_404(Poll, pk=pk)
-        data = PollSerializer(poll).data
-        return Response(data)
+# class PollDetail(APIView):
+#     def get(self, request, pk):
+#         poll = get_object_or_404(Poll, pk=pk)
+#         data = PollSerializer(poll).data
+#         return Response(data)
+
+"""
+ListCreateAPIView - get a list of entities or create them. 
+Allow GET and POST
+"""
+class PollList(generics.ListCreateAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+"""
+RetriveDestroyAPIView - retrive an individual entity details or delete the entity
+Allows GET and DELETE
+"""
+class PollDetail(generics.RetrieveDestroyAPIView):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+
+"""
+CreateAPIView - Allows creating entities, but not listing them
+Allows POST
+"""
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
